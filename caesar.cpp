f2776f9a05f26e21ead2ac3e8c0a94f7f25b7e0c
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-10-04 13:52:56 -0400

    Edited const to be uniform with vigenere

--- caesar.cpp
+++ caesar.cpp
@@ -3,8 +3,11 @@
 #include <string>   
 
 
-const std::array<char, 26> upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-const std::array<char, 26> lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
+const std::array<char, 26> upper = 
+{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+const std::array<char, 26> lower = 
+{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
 
 char shiftChar(char c, int rshift) {  
     int shift = rshift % 26; 
