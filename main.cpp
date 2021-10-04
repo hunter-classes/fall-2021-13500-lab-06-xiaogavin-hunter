1fa83b8603e8758d187322ab86c902ecafdc4378
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-10-04 13:53:54 -0400

    Used for testing vigenere

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "funcs.h"
 #include "caesar.h"
+#include "vigenere.h"
 
 int main() {
-    // std::cout << << std::endl;
+    std::cout << encrypt_vigenere("Hello, World!", "cake") << std::endl;
 
     return 0;
 }
