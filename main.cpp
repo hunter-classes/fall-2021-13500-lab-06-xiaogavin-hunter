4460919246b105133389ec722163847066966180
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-10-07 11:37:20 -0400

    Added decryption tests, using the other tasks

--- main.cpp
+++ main.cpp
@@ -6,25 +6,44 @@
 #include "decrypt.h"
 
 int main() {
-    // std::cout << "Enter plaintext: ";
-    // std::string plaintext; 
+    std::cout << "Enter plaintext: Hello, World!";
+    std::string plaintext = "Hello, World!"; 
     // std::getline(std::cin, plaintext); 
 
-    // std::cout << "\n= Caesar =" << std::endl;
-    // std::cout << "Enter shift" << std::setw(6) << ": "; 
-    // int shift; 
+    std::cout << "\n= Caesar =" << std::endl;
+    std::cout << "Enter shift" << std::setw(8) << ": 10" << std::endl; 
+    int shift = 10; 
     // std::cin >> shift; 
-    // std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_caesar(plaintext, shift) << std::endl;
-    // std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_caesar(encrypt_caesar(plaintext, shift), shift) << std::endl;
+    std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_caesar(plaintext, shift) << std::endl;
+    std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_caesar(encrypt_caesar(plaintext, shift), shift) << std::endl;
 
-    // std::cout << "\n= Vigenere =" << std::endl;
-    // std::cout << "Enter keyword" << std::setw(4) << ": "; 
-    // std::string keyword; 
+    std::cout << "\n= Vigenere =" << std::endl;
+    std::cout << "Enter keyword" << std::setw(8) << ": cake" << std::endl; 
+    std::string keyword = "cake"; 
     // std::cin >> keyword; 
-    // std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_vigenere(plaintext, keyword) << std::endl; 
-    // std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_vigenere(encrypt_vigenere(plaintext, keyword), keyword) << std::endl;
+    std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_vigenere(plaintext, keyword) << std::endl; 
+    std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_vigenere(encrypt_vigenere(plaintext, keyword), keyword) << std::endl;
 
-    std::cout << "I " << get_position_in_array('I') << " Q " << get_position_in_array('Q') << std::endl;
+    std::cout << "\n ============================================ \n"; 
+
+    std::cout << "Enter plaintext: Lab 06 is Complete";
+    plaintext = "Lab 06 is Complete"; 
+    // std::getline(std::cin, plaintext); 
+
+    std::cout << "\n= Caesar =" << std::endl;
+    std::cout << "Enter shift" << std::setw(8) << ": 30" << std::endl; 
+    shift = 30; 
+    // std::cin >> shift; 
+    std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_caesar(plaintext, shift) << std::endl;
+    std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_caesar(encrypt_caesar(plaintext, shift), shift) << std::endl;
+
+    std::cout << "\n= Vigenere =" << std::endl;
+    std::cout << "Enter keyword" << std::setw(8) << ": Hello!" << std::endl; 
+    keyword = "Hello!"; 
+    // std::cin >> keyword; 
+    std::cout << "Ciphertext" << std::setw(7) << ": " << encrypt_vigenere(plaintext, keyword) << std::endl; 
+    std::cout << "Decrypted" << std::setw(8) << ": " << decrypt_vigenere(encrypt_vigenere(plaintext, keyword), keyword) << std::endl;
+
 
     return 0;
 }
